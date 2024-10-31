9706f38e4cb20ab7bf0e5617a2e944c1415b1fe5
minnji-kim <164763162+minnji-kim@users.noreply.github.com>
2024-11-01 06:31:03 +0900

    Update App.js

--- fitgpt/src/App.js
+++ fitgpt/src/App.js
@@ -1,25 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
 import Calendar from './Calendar';
 import Main from './Main';
 import Chat from "./Chat";
 import Recommend from "./Recommend";
-import Siginin from "./Signin";
-import Siginup from "./Signup";
-
+import Signin from './Siginin';
+import Signup from "./Siginup";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/Write" element={<Calendar />} />
-          <Route path="/Chat" element={<Chat />} />
-          <Route path="/Recommend" element={<Recommend />} />
-          <Route path="/signin" element={<Siginin />} />
-          <Route path="/signup" element={<Siginup />} />
-        </Routes>
-      </BrowserRouter>
+     <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/Write" element={<Calendar />} />
+        <Route path="/Chat" element={<Chat />} />
+        <Route path="/Recommend" element={<Recommend />} />
+        <Route path = "/signin" element={<Signin/>}/>
+        <Route path = "/signup" element={<Signup/>}/>
+      </Routes>
     </div>
   );
 }
