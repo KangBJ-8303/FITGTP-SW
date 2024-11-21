51f15b270fc6de17708b9453601811a824cafef5
minnji-kim <164763162+minnji-kim@users.noreply.github.com>
2024-11-21 15:40:40 +0900

    Update atoms.js

--- fitgpt/src/atoms.js
+++ fitgpt/src/atoms.js
@@ -1,6 +1,12 @@
+// atoms.js
 import { atom } from 'recoil';
 
 export const userEmailState = atom({
-  key: 'userEmailState', // 고유한 key 값
+  key: 'userEmailState',
   default: null, // 초기 값은 null (로그인 전)
+});
+
+export const userWorkoutRecordsState = atom({
+  key: 'userWorkoutRecordsState',
+  default: [], // 초기 값은 빈 배열
 });
